03d11b32386fd663046eccc98f7e142e5143182a
Johny Lima <johny.lima@hexa-consulting.pt>
2023-06-27 00:40:59 +0100

    LB

--- setup.sh
+++ setup.sh
@@ -5,12 +5,3 @@ kubectl create namespace phone-validator-frontend
 
 echo Deploy phone-validator-frontend
 kubectl apply -f phone-validator-frontend-deployment.yaml -n phone-validator-frontend 
-
-
-
-kubectl apply -f model1.yaml -n 2048-game
-kubectl apply -f model2.yaml -n 2055-Test
-
-kubectl delete -f model1.yaml 2048-game 
-
-kubectl delete all -l app=phone-validator-backend
