c57675acfca3218cd045673fc414dd1ca799f5e4
Johny Lima <johny.lima@hexa-consulting.pt>
2023-06-27 16:37:30 +0100

    lb

--- setup.sh
+++ setup.sh
@@ -4,4 +4,5 @@ echo Create namespace for phone-validator-backend
 kubectl create namespace phone-validator-backend
 
 echo Deploy phone-validator-backend
-kubectl apply -f phone-validator-backend-deployment.yaml -n phone-validator-backend 
+kubectl apply -f phone-validator-backend-deployment.yaml -n phone-validator-backend
+kubectl get all -n phone-validator-backend
--- teardown.sh
+++ teardown.sh
@@ -2,3 +2,5 @@
 
 kubectl delete -f phone-validator-backend-deployment.yaml -n phone-validator-backend  
 kubectl delete ns phone-validator-backend
+
+#kubectl delete all -l app=phone-validator-backend -n phone-validator-backend
