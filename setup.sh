20de32de21500d2af80d21393908b7034417be9e
Johny Lima <johny.lima@hexa-consulting.pt>
2023-07-04 12:23:43 +0100

    lb

--- setup.sh
+++ setup.sh
@@ -4,7 +4,15 @@ echo Create namespace for phone-validator-frontend
 kubectl create namespace phone-validator-frontend
 
 echo Deploy phone-validator-frontend
+# loadbalance
 kubectl apply -f phone-validator-frontend-deployment.yaml -n phone-validator-frontend 
-kubectl apply -f phone-validator-frontend-deployment-ingress.yaml -n phone-validator-frontend 
+
+# alb
+kubectl apply -f phone-validator-frontend-deployment-alb-ingress.yaml -n phone-validator-frontend
+
+# nginx
+kubectl apply -f phone-validator-frontend-deployment-nginx-ingress.yaml -n phone-validator-frontend 
 
 kubectl get all -n phone-validator-frontend 
+kubectl get pods -o wide -n phone-validator-frontend
+kubectl get svc ingress-nginx-controller -n ingress-nginx -ojson
